d3e315a8e9a0f2b356194f41ca89a14d373019cf
atanas <atanas.atanas@gmail.com>
2020-07-12 22:09:20 +1000

    add private config layer documents and settings

---
+++ +init-custom.el
@@ -0,0 +1,276 @@
+(use-package! spss
+  :load-path "./spss-mode"
+  :init
+  (require 'spss)
+  )
+
+;; ===========================================================
+;; Python
+;; ===========================================================
+
+;; (use-package! elpy
+;;   :after elpy
+;;   :ensure t
+;;   ;; :defer t
+;;   :init
+;;   (defun enable-elpy-once ()
+;;     (elpy-enable)
+;;     (advice-remove 'python-mode 'enable-elpy-once))
+;;   (advice-add 'python-mode :before 'enable-elpy-once)
+;;   :config
+;;   ;; enable jupyter
+;;   (setq python-shell-interpreter "jupyter"
+;;         python-shell-interpreter-args "console --simple-prompt"
+;;         python-shell-prompt-detect-failure-warning nil
+;;         elpy-rpc-python-command "/usr/local/bin/python3")
+;;   (add-to-list 'python-shell-completion-native-disabled-interpreters
+;;                "jupyter")
+;;   ;; enable flycheck
+;;   (when (require 'flycheck nil t)
+;;     (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+;;     (add-hook 'elpy-mode-hook 'flycheck-mode))
+;;   )
+;; ;; Enable autopep8
+;; (use-package! py-autopep8
+;;   :after python elpy
+;;   :config
+;;   (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+;;   )
+
+;; ===========================================================
+;; Visual-Changes
+;; ===========================================================
+
+(use-package! all-the-icons-ivy-rich
+  :after (counsel)
+  :init (all-the-icons-ivy-rich-mode 1))
+
+(use-package! ivy-rich
+  :after all-the-icons-ivy-rich
+  :init (ivy-rich-mode 1))
+
+(use-package! all-the-icons-ibuffer
+  :init (all-the-icons-ibuffer-mode 1))
+;; colorise colour references
+;; (use-package! rainbow-mode
+;;   :config (rainbow-mode t))
+
+;; (add-hook 'text-mode-hook #'visual-line-mode)
+;; (add-hook 'text-mode-hook #'display-line-numbers-mode)
+;; (add-hook 'prog-mode-hook #'visual-line-mode)
+;; (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+;; (add-hook 'prog-mode-hook #'auto-fill-mode)
+;; (add-hook 'occur-mode-hook #'visual-line-mode)
+;; (add-hook 'spss-mode-hook #'visual-line-mode)
+;; (add-hook 'spss-mode-hook #'display-line-numbers-mode)
+;; (add-hook 'spss-mode-hook #'auto-fill-mode)
+
+;; ;; the hs-mode made things too stuttery in rmd mode
+;; (add-hook 'markdown-mode-hook #'hs-minor-mode)
+;; (add-hook 'text-mode-hook #'rainbow-delimiters-mode)
+;; (setq-default display-line-numbers-width 3)
+
+;; (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+;; (add-to-list 'default-frame-alist '(fullscreen . fullheight))
+
+;; (with-eval-after-load 'dashboard
+;;   (setq dashboard-banner-logo-title "\"If you can't explain it simply, you don't understand it well enough.\""
+;;         dashboard-set-footer nil
+;;         dashboard-foot-icon nil
+;;         dashboard-footer ""
+;;         dashboard-items nil
+;;         dashboard-set-navigator nil))
+
+;; ===========================================================
+;; aj/defun
+;; ===========================================================
+
+;; from https://www.emacswiki.org/emacs/BackwardKillLine kill line backwards
+(defun aj/backward-kill-line (arg)
+  "Kill ARG lines backward."
+  (interactive "p")
+  (kill-line (- 1 arg)))
+
+(global-set-key "\C-cu" 'aj/backward-kill-line) ;; `C-c u'
+
+;; iTerm2 intergration
+;; from https://sam217pa.github.io/2016/09/01/emacs-iterm-integration/
+;; return the directory of the file currently opened. If it is a *scratch* buffer or something like that, it simply returns the home directory
+(defun aj/get-file-dir-or-home ()
+  "If inside a file buffer, return the directory, else return home"
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        "~/"
+      (file-name-directory filename))))
+
+;; cd to the directory of the file I am editing in emacs. If I am in a *scratch* buffer or something like that, it cd to the $HOME directory. It then focus the iTerm2 app.
+(defun aj/iterm-goto-filedir-or-home ()
+  "Go to present working dir and focus iterm"
+  (interactive)
+  (do-applescript
+   (concat
+    " tell application \"iTerm2\"\n"
+    "   tell the current session of current window\n"
+    (format "     write text \"cd %s\" \n" (aj/get-file-dir-or-home))
+    "   end tell\n"
+    " end tell\n"
+    " do shell script \"open -a iTerm\"\n"
+    ))
+  )
+;; focus the iTerm2 app, without modifying the working directory
+(defun aj/iterm-focus ()
+  (interactive)
+  (do-applescript
+   " do shell script \"open -a iTerm\"\n"))
+
+(global-set-key (kbd "C-c t h") 'aj/iterm-goto-filedir-or-home)
+(global-set-key (kbd "C-c t n") 'aj/iterm-focus)
+
+(add-hook 'markdown-mode-hook
+          '(lambda ()
+             (setq flyspell-generic-check-word-predicate 'aj/pandoc-flyspell-verify)))
+
+(defun aj/pandoc-flyspell-verify ()
+  (save-excursion
+    (forward-word -1)
+    (not (looking-back "@"))))
+
+;; pandoc wordcount
+(defun aj/pandoc-wc ()
+  "Return wordcount of current buffer using pandoc wordcount.lua"
+  (interactive) (shell-command (concat "pandoc --lua-filter wordcount.lua " buffer-file-name)))
+;; this might be problematic, as really I only want this for markdown docs
+
+;; preview markdownf file in marked2.app
+(defun aj/markdown-preview-file ()
+  "run Marked on the current file and revert the buffer"
+  (interactive)
+  (shell-command
+   (format "open -a /Applications/Marked\\ 2.app %s" (shell-quote-argument (buffer-file-name)))))
+
+(global-set-key "\C-cm" 'aj/markdown-preview-file)
+
+;; with courtesy from https://emacs.stackexchange.com/questions/54939/how-do-i-get-request-el-to-post-the-contents-into-the-buffer/54950?noredirect=1#comment85788_54950
+(defun aj/zotero-cayw ()
+  "Insert Zotero Cite at point."
+  (interactive)
+  (insert
+   (shell-command-to-string
+    "curl -s http://localhost:23119/better-bibtex/cayw?format=pandoc"))
+  (do-applescript "tell application \"Emacs\" to activate"))
+
+(global-set-key (kbd "C-s-y") 'aj/zotero-cayw)
+
+;; ===========================================================
+;; Keybindings
+;; ===========================================================
+
+;; (with-eval-after-load 'undo-tree
+;;   ;; make undotree work as expected
+;;   (define-key undo-tree-map (kbd "C-/") 'undo-tree-undo)
+;;   (define-key undo-tree-map (kbd "C-_") nil)
+;;   (define-key undo-tree-map (kbd "C-?") 'undo-tree-redo)
+;;   (define-key undo-tree-map (kbd "M-_") nil)
+;;   (define-key undo-tree-map (kbd "C-z") 'undo-tree-undo)
+;;   (define-key undo-tree-map (kbd "C-S-z") 'undo-tree-redo))
+
+;; (with-eval-after-load 'drag-stuff
+;;   ;; make drag-stuff less annoying
+;;   (define-key drag-stuff-mode-map (kbd "<M-left>") nil)
+;;   (define-key drag-stuff-mode-map (kbd "<M-right>") nil))
+
+(with-eval-after-load 'ivy
+  (define-key ivy-minibuffer-map (kbd "C-SPC") 'ivy-mark)
+  (define-key ivy-minibuffer-map (kbd "C-S-SPC") 'ivy-unmark))
+
+;; (with-eval-after-load 'company
+;;   (define-key company-mode-map (kbd "C-M-s-/") 'company-files))
+
+;; ===========================================================
+;; Miscellaneous
+;; ===========================================================
+
+(use-package! flyspell-lazy
+  :after flyspell
+  :config
+  (flyspell-lazy-mode 1))
+
+;; (add-hook 'text-mode-hook
+;;           (lambda (flyspell-mode -1)))
+;; (add-hook 'outline-mode-hook
+;;           (lambda (flyspell-mode -1)))
+;; (add-hook 'prog-mode-hook
+;;           (lambda (flyspell-prog-mode -1)))
+
+;; (add-hook 'prog-mode-hook #'wucuo-start)
+;; (add-hook 'text-mode-hook #'wucuo-start)
+
+;; stop asking for mc to confit multiple changes
+(setq mc/always-run-for-all t)
+
+;; setup grip-mode
+(with-eval-after-load 'grip
+  ;; Path to the grip binary
+  (setq grip-binary-path "/usr/local/bin/grip")
+
+  ;; after every text change
+  (setq grip-update-after-change nil)
+
+  ;; Use embedded webkit to preview
+  (setq grip-preview-use-webkit t)
+
+  (require 'auth-source)
+  (let ((credential (auth-source-user-and-password "api.github.com")))
+    (setq grip-github-user (car credential)
+          grip-github-password (cadr credential))))
+
+
+;; add custom hl-todos and set colours
+(with-eval-after-load 'hl-todo
+  (add-to-list 'hl-todo-keyword-faces '("ANCHOR" . "#DAF7A6"))
+  (add-to-list 'hl-todo-keyword-faces '("REVIEW" . "#5eff33"))
+  (add-to-list 'hl-todo-keyword-faces '("NOTE" . "#ff8e33"))
+  ;; (add-to-list 'hl-todo-keyword-faces '("TODO" . "#ff3349"))
+  (add-to-list 'hl-todo-keyword-faces '("SYNOPSIS" . "#4fd4ff"))
+  (setq hl-todo-include-modes '(prog-mode text-mode markdown-mode))
+  )
+
+;; NOTE this is not workiing as expected
+;; TODO remove this code or correct config
+;; add to magit status view
+;; (with-eval-after-load 'magit
+;;   (magit-todos-mode -1)
+;;   )
+
+;; this works well for small screens
+(with-eval-after-load 'lsp-ui-mode
+  (setq lsp-ui-doc-position 'top))
+
+;; this makes lsp-mode work much snappier
+(with-eval-after-load 'lsp-mode
+  (setq lsp-idle-delay 0.5
+        lsp-links-check-internal 0.9
+        lsp-prefer-capf t
+        lsp-ui-sideline-delay 0.9)
+  )
+
+;; savehist was maxing cpu
+(with-eval-after-load 'savehist-mode
+  (setq history-length 10)
+  ;; (savehist-mode nil)
+  )
+
+;; (with-eval-after-load 'org-roam-mode
+;;   (remove-hook 'after-init-hook 'org-roam-mode-hook t)
+;;   )
+
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "<return>") nil)
+  (define-key company-active-map (kbd "RET") nil)
+  (define-key company-active-map (kbd "<tab>") #'company-complete-selection))
+
+(provide 'init-custom-post)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-custom-post.el ends here
---
+++ +init-writing.el
@@ -0,0 +1,180 @@
+;;; init-package.el --- Initialize package configurations.	-*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Atanas Janackovski
+
+;; Author: Atanas Janackovski <atanas.atanas@gmail.com>
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+
+;;; Commentary:
+;;
+;; Emacs Package configurations template.
+;;
+
+;;; Code:
+
+;; (require 'init-const)
+;; (require 'init-custom)
+;; (require 'init-funcs)
+
+
+;; ===========================================================
+;; Writing stuff
+;; ===========================================================
+
+;; pandoc setup
+(use-package! pandoc-mode
+  :init
+  (add-hook 'pandoc-mode-hook 'pandoc-load-default-settings)
+  :hook
+  (markdown-mode . pandoc-mode)
+  :bind
+  ("C-s-j" . pandoc-jump-to-reference)
+  :config
+  (setq pandoc-data-dir "/Users/atanas/.emacs.d/site-lisp/pandoc-mode/"
+        pandoc-citation-jump-function 'pandoc-open-in-ebib))
+
+;; setup ebib to work with pandoc and more
+(use-package! ebib
+  :config
+  (setq ebib-index-columns
+        '(("Entry Key" 24 t)
+          ("Author/Editor" 40 t)
+          ("Year" 6 t)
+          ("Title" 66 t)))
+  (setq ebib-preload-bib-files '("/Users/atanas/.pandoc/MyLib.bib" "/Users/atanas/OneDrive - Grand Pacific Health Ltd/GPH/GPH.bib")
+        ebib-layout 'index-only
+        ebib-file-associations '(("pdf" . "open")
+                                 ("ps" . "gv")))
+  (setq ebib-citation-commands
+        '((latex-mode
+           (("cite" "\\cite%<[%A]%>[%A]{%(%K%,)}")
+            ("paren" "\\parencite%<[%A]%>[%A]{%(%K%,)}")
+            ("foot" "\\footcite%<[%A]%>[%A]{%(%K%,)}")
+            ("text" "\\textcite%<[%A]%>[%A]{%(%K%,)}")
+            ("smart" "\\smartcite%<[%A]%>[%A]{%(%K%,)}")
+            ("super" "\\supercite{%K}")
+            ("auto" "\\autocite%<[%A]%>[%A]{%(%K%,)}")
+            ("cites" "\\cites%<(%A)%>(%A)%(%<[%A]%>[%A]{%K}%)")
+            ("parens" "\\parencites%<(%A)%>(%A)%(%<[%A]%>[%A]{%K}%)")
+            ("foots" "\\footcites%<(%A)%>(%A)%(%<[%A]%>[%A]{%K}%)")
+            ("texts" "\\textcites%<(%A)%>(%A)%(%<[%A]%>[%A]{%K}%)")
+            ("smarts" "\\smartcites%<(%A)%>(%A)%(%<[%A]%>[%A]{%K}%)")
+            ("supers" "\\supercites%<(%A)%>(%A)%(%<[%A]%>[%A]{%K}%)")
+            ("autos" "\\autoscites%<(%A)%>(%A)%(%<[%A]%>[%A]{%K}%)")
+            ("author" "\\citeauthor%<[%A]%>[%A]{%(%K%,)}")
+            ("title" "\\citetitle%<[%A]%>[%A]{%(%K%,)}")
+            ("year" "\\citeyear%<[%A]%>[%A][%A]{%K}")
+            ("date" "\\citedate%<[%A]%>[%A]{%(%K%,)}")
+            ("full" "\\fullcite%<[%A]%>[%A]{%(%K%,)}")))
+          (org-mode
+           (("ebib" "[[ebib:%K][%D]]")))
+          (markdown-mode
+           (("text" "@%K%< [%A]%>")
+            ("paren" "[%<%A %>%(@%K%; )%<, %A%>]")
+            ("year" "[-@%K%< %A%>]")))))
+  (setq ivy-re-builders-alist '((t . ivy-prescient-non-fuzzy)))
+  :hook
+  (ebib-entry-mode . visual-line-mode)
+  :bind
+  ((:map markdown-mode-map
+    ("M-s-b" . ebib-insert-citation)
+    ("C-c M-s-b" . ebib))
+   (:map org-mode-map
+    ("M-s-b" . ebib-insert-citation)
+    ("C-c M-s-b" . ebib))
+   (:map LaTeX-mode-map
+    ("M-s-b" . ebib-insert-citation)
+    ("C-c M-s-b" . ebib))))
+
+;; distraction-free writing
+(use-package! writeroom-mode
+  :config
+  ;; make big then make small again
+  ;; not working consistently, so removed
+  ;; (add-hook 'writeroom-mode-hook #'(lambda () (text-scale-increase 2)))
+  ;; (add-hook 'writeroom-mode-disable-hook #'(lambda () (text-scale-decrease 4)))
+  (setq writeroom-fullscreen-effect 'maximized
+        writeroom-extra-line-spacing 0.8)
+  :bind (("C-c C-w C-r" . writeroom-mode)
+         :map writeroom-mode-map
+         ("C-M-<" . writeroom-decrease-width)
+         ("C-M->" . writeroom-increase-width)
+         ("C-M-=" . writeroom-adjust-width)))
+
+;; identify slopy prose
+(use-package! writegood-mode)
+
+;; use poper english
+(setq ispell-dictionary "british")
+
+;; ===========================================================
+;; Latex-IDE
+;; ===========================================================
+
+;; continous compilation for syncing preview with changes
+(use-package! auctex-latexmk)
+
+(use-package! tex
+  :ensure auctex
+  :config
+  ;; Indent items by two spaces.
+  (setq LaTeX-item-indent 0)
+  (setq TeX-PDF-mode t)
+  ;; Generate sync file and sync with C-v
+  (eval-after-load
+      "tex" '(add-to-list 'TeX-command-list
+                          '("latexmk" "latexmk -pdf %t --synctex=1" TeX-run-TeX)))
+  (setq latex-run-command "pdflatex")
+  (setq LaTeX-command "latex --synctex=1")
+  ;; Use pdf-tools to open PDF files
+  (setq TeX-view-program-selection '((output-pdf "open")) ; "Skim" "open" "PDF Tools"
+        TeX-source-correlate-start-server t
+        TeX-source-correlate-method 'synctex)
+  (setq TeX-auto-save t
+        TeX-parse-self t)
+  ;; Needed to sync TeX and PDF
+  (add-hook 'LaTeX-mode-hook
+            '(lambda ()
+               (TeX-source-correlate-mode 1)))
+  (add-hook 'TeX-after-compilation-finished-functions
+            #'TeX-revert-document-buffer)
+  ;; (add-hook 'pdf-view-mode-hook 'auto-revert-mode)
+  ;; (setq auto-revert-interval 0.5)
+  (add-hook 'pdf-view-mode-hook
+            (lambda ()
+              (pdf-view-fit-page-to-window) ))
+  (add-hook 'LaTeX-mode-hook
+            '(lambda ()
+               (reftex-mode))))
+
+(use-package! company-auctex
+  :ensure t
+  :init (company-auctex-init))
+
+(use-package! reftex
+  :ensure t
+  :defer t
+  :config
+  (setq reftex-cite-prompt-optional-args t)); Prompt for empty optional arguments in cite
+
+(provide '+init-writing)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; +init-writing.el ends here
